e9201f0fc2233a9efc7a173561ecf5c7edd8ea9d
Valeria Kliui <valeriakliui@gmail.com>
2024-04-17 17:24:45 +0300

    fix: fix vite config for publishing

--- src/main.tsx
+++ src/main.tsx
@@ -13,20 +13,23 @@ import { OrderPage } from "./pages/OrderPage";
 import { PoliticsPage } from "./pages/PoliticsPage";
 import { GlobalStyles } from "./styled";
 
-const router = createBrowserRouter([
-  {
-    element: <PageLayout />,
-    path: PATHS.MAIN,
-    children: [
-      { element: <MainPage />, index: true },
-      { element: <PoliticsPage />, path: "politics" },
-      {
-        path: PATHS.ORDER,
-        element: <OrderPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <PageLayout />,
+      path: PATHS.MAIN,
+      children: [
+        { element: <MainPage />, index: true },
+        { element: <PoliticsPage />, path: "politics" },
+        {
+          path: PATHS.ORDER,
+          element: <OrderPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/izobox" },
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
